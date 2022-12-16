1118ad4edfb01f3ca8e05a4f4b67892597716d1b
Helena Tenev <htenev14@gmail.com>
2022-12-15 22:08:14 -0500

    commit tree dfed4baca40e7508048756e71440b569d7429edd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() { 
+    document.getElementById('text').textContent = 'This is really cool!';
+//   const text = document.getElementById("text");
+//   document.node.textContent=document.node.textContent.replace(text.textContent, "This is really cool!")
+});
+
+// var x = document.getElementsByClassName("q");
+// for (var i = 0; i < x.length; i++) {
+//     document.body.innerHTML = document.body.innerHTML.replace(x[i].innerHTML, 'Hello World');
+// }
+// document.body.textContent=document.body.textContent.replace(text.textContent, "This is really cool!")
+
+
